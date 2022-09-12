6a505a5ee0217d4398b9ecf1402dea315f065db6
Richard <richard031104@outlook.com>
2022-09-11 22:34:28 -0400

    Title/Icon Edit

--- deploy.sh
+++ deploy.sh
@@ -21,6 +21,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:MetalRick123299/Restarone-Challenge.git main:gh-pages
+git push -f git@github.com:MetalRick123299/Company-Dashboard.git main:gh-pages
 
 cd -
